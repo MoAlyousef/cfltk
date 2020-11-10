5b07b7bfb077deb85ecee7464574d5329a802d75
MoAlyousef <may642_2000@hotmail.com>
2020-11-10 11:07:53 +0300

    first commit

---
+++ include/cfl_box.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "cfl_widget.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+WIDGET_DECLARE(Fl_Box)
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/cfl_box.cpp
@@ -0,0 +1,8 @@
+#include "cfl_box.h"
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Image.H>
+
+WIDGET_CLASS(Fl_Box)
+
+WIDGET_DEFINE(Fl_Box)
