e12181510d2985497d44643ea81788a9d2a4dca7
MoAlyousef <may642_2000@hotmail.com>
2021-03-17 12:21:47 +0300

    formatting

--- include/cfl_menu.h
+++ include/cfl_menu.h
@@ -113,7 +113,8 @@ void *Fl_Menu_Item_user_data(const Fl_Menu_Item *);
 
 void Fl_Menu_Item_set_user_data(Fl_Menu_Item *, void *data);
 
-void Fl_Menu_Item_draw(const Fl_Menu_Item *self, int x, int y, int w, int h, const void *m, int selected);
+void Fl_Menu_Item_draw(const Fl_Menu_Item *self, int x, int y, int w, int h, const void *m,
+                       int selected);
 
 int Fl_Menu_Item_measure(const Fl_Menu_Item *self, int *hp, const void *m);
 
