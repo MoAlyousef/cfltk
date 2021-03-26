8867e5ad46ec040b583b33a01a0e65bc61182ac3
MoAlyousef <may642_2000@hotmail.com>
2021-03-26 13:56:07 +0300

    add default selection and inactive colors

--- include/cfl.h
+++ include/cfl.h
@@ -173,6 +173,10 @@ void Fl_background(unsigned char r, unsigned char g, unsigned char b);
 
 void Fl_background2(unsigned char r, unsigned char g, unsigned char b);
 
+void Fl_selection_color(unsigned char r, unsigned char g, unsigned char b);
+
+void Fl_inactive_color(unsigned char r, unsigned char g, unsigned char b);
+
 void Fl_get_system_colors(void);
 
 int Fl_handle(int, void *);
