ac2119a608730edb324cff7954c31bb77a18f1f6
MoAlyousef <may642_2000@hotmail.com>
2022-12-23 00:23:25 +0300

    link libsupc++ with gnu toolchain

--- src/cfl_new.cpp
+++ src/cfl_new.cpp
@@ -19,27 +19,3 @@ void operator delete(void *val) noexcept {
 void operator delete[](void *val) noexcept {
     free(val);
 }
-
-#ifndef __ANDROID__
-void *__gxx_personality_v0 = 0;
-#endif
-
-extern "C" int __cxa_guard_acquire(long *g) {
-    return !*(char *)(g);
-}
-
-extern "C" void __cxa_guard_release(long *g) {
-    *(char *)g = 1;
-}
-
-extern "C" void __cxa_pure_virtual() {
-    // Do nothing
-}
-
-extern "C" void __cxa_guard_abort(long *) {
-    // Do nothing
-}
-
-extern "C" void __cxa_atexit() {
-    // Do nothing
-}
