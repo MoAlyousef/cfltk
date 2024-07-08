4d16c7f5cdcf8326e9fd85a4438f4254db843627
Mohammed Alyousef <may642_2000@hotmail.com>
2024-07-09 01:30:33 +0300

    update tree code

--- src/cfl_tree.cpp
+++ src/cfl_tree.cpp
@@ -418,7 +418,7 @@ void *Fl_Tree_openicon(const Fl_Tree *self) {
 void Fl_Tree_set_openicon(Fl_Tree *self, void *val) {
     LOCK(auto old = self->openicon(); if (!val) self->openicon(nullptr);
          else self->openicon(((Fl_Image *)val)->copy());
-         delete old;)
+         )
 }
 
 void *Fl_Tree_closeicon(const Fl_Tree *self) {
@@ -432,7 +432,7 @@ void *Fl_Tree_closeicon(const Fl_Tree *self) {
 void Fl_Tree_set_closeicon(Fl_Tree *self, void *val) {
     LOCK(auto old = self->closeicon(); if (!val) self->closeicon(nullptr);
          else self->closeicon(((Fl_Image *)val)->copy());
-         delete old;)
+         )
 }
 
 int Fl_Tree_showcollapse(const Fl_Tree *self) {
